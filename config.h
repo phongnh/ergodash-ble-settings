932c030baff7217aea7daaf7fbb5225ccfddf959
Phong Nguyen <nhphong1406@gmail.com>
2019-10-28 02:19:04 +0700

    Add BLE settings

--- config.h
+++ config.h
@@ -1,5 +1,15 @@
 #pragma once
 
+// BLE settings
+#undef BLE_NUS_MIN_INTERVAL
+#undef BLE_NUS_MAX_INTERVAL
+#undef BLE_HID_MAX_INTERVAL
+#undef BLE_HID_SLAVE_LATENCY
+#define BLE_NUS_MIN_INTERVAL  30
+#define BLE_NUS_MAX_INTERVAL  60
+#define BLE_HID_MAX_INTERVAL  80
+#define BLE_HID_SLAVE_LATENCY 10
+
 // Copy from ErgoDox EZ
 #define MOUSEKEY_INTERVAL    20
 #define MOUSEKEY_DELAY       0
